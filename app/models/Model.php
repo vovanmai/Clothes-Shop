5f7f002a76c42b3e468102746c7ac2e22aa0caac
luonghieu <luonghieu.t3@gmail.com>
2017-10-18 00:04:29 +0700

    Update Model.php

--- app/models/Model.php
+++ app/models/Model.php
@@ -25,16 +25,23 @@ class Model
 		return App::get('database')->query_fetch($query);
 	}
 	public static function find($id)
-    $query="SELECT * FROM ".static::$table." WHERE id = ?";
-		return App::get('database')->query_fetch_params($query,array('id' => id));
-	}
-
-
-	public static function find($column, $value)
 	{
-		$query="SELECT * FROM ".static::$table." WHERE ". $column. " = ?";
-		return App::get('database')->query_fetch_params($query,array($column => $value));
+		$query="SELECT * FROM ".static::$table." WHERE id = ?";
+		return App::get('database')->query_fetch_params($query,array('id' => $id));
 	}
+
+	// public static function find($column, $value)
+	// {
+	// 	$query="SELECT * FROM ".static::$table." WHERE ". $column. " = ?";
+	// 	return App::get('database')->query_fetch_params($query,array($column => $value));
+	// }
+
+
+	// public static function find($column, $value)
+	// {
+	// 	$query="SELECT * FROM ".static::$table." WHERE ". $column. " = ?";
+	// 	return App::get('database')->query_fetch_params($query,array($column => $value));
+	// }
 
 	public static function insert($parameters)
 	{	
@@ -63,5 +70,6 @@ class Model
 		$query="DELETE FROM ".static::$table." WHERE id=?";
 		return App::get('database')->query_excute_params($query,array('id'=>$id));
 	}
+
 }
 ?>
