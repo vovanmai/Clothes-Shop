163924abc6728ed48fe509a1a97204f95b1552f9
daipham1210 <dai.pd@neo-lab.vn>
2017-10-18 08:31:40 +0700

    +add public view

--- app/views/public/require/header.view.php
+++ app/views/public/require/header.view.php
@@ -1,164 +1,165 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title>Shop</title>
-	<link rel="stylesheet" type="text/css" href="/public/assets/css/font-awesome.min.css">
-	<link rel="stylesheet" href="/public/assets/css/style.css">
+    <title>Shop</title>
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/public/assets/css/style.css">
 </head>
+
 <body>
 
-	<div class="wrapper">
-		<header id="header">
-			<nav class="container" id="navbar">
-				<div class="navbar-full hidden-mobile hidden-tablet row">
-					<div class=" col-md-2 col-lg-3">
-						<div class="logo-shop cover">
-							<p>Fashion & <span>Style</span></p>
-						</div>
-					</div>
+    <div class="wrapper">
+        <header id="header">
+            <nav class="container" id="navbar">
+                <div class="navbar-full hidden-mobile hidden-tablet row">
+                    <div class=" col-md-2 col-lg-3">
+                        <div class="logo-shop cover">
+                            <a href="index.html"><p>Fashion & <span>Style</span></p></a>
+                        </div>
+                    </div>
 
-					<div class="col-md-5 col-lg-4">
-						<div class="cover menu">
-							<ul>
-								<li><a class="active" href="index.html">Home</a></li>
-								<li>
-									<a href="men.html">Men</a>
-									<div class="menu-child">
-										<ul>
-											<li>
-												<div href="">Shirt</div>
-												<ul>
-													<li><a href="">T-Shirt</a></li>
-													<li><a href="">Shirt</a></li>
-													<li><a href="">Somi</a></li>
-												</ul>
-											</li>
-											<li>
-												<div href="">Trousers</div>
-												<ul>
-													<li><a href="">Jean</a></li>
-													<li><a href="">Khaki</a></li>
-												</ul>
-											</li>
+                    <div class="col-md-5 col-lg-4">
+                        <div class="cover menu">
+                            <ul>
+                                <li><a class="active" href="index.html">HOME</a></li>
+                                <li>
+                                    <a href="men.html">MEN</a>
+                                    <div class="menu-child">
+                                        <ul>
+                                            <li>
+                                                <a href="">T-Shirt</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Shirt</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Trousers</a>
+                                            </li>
 
-											<li>
-												<div href="">Jacket</div>
-												<ul>
-													<li><a href="">Jean</a></li>
-													<li><a href="">Kaki</a></li>
-												</ul>
-											</li>
-										</ul>
-										<div class="box-img">
-											<img src="img/men-logo.jpeg" alt="">
-										</div>
-									</div>
-									
-								</li>
-								<li>
-									<a href="women.html">Women</a>
-									<div class="menu-child">
-										<ul>
-											<li>
-												<div href="">Shirt</div>
-												<ul>
-													<li><a href="">T-Shirt</a></li>
-													<li><a href="">Shirt</a></li>
-													<li><a href="">Somi</a></li>
-												</ul>
-											</li>
-											<li>
-												<div href="">Trousers</div>
-												<ul>
-													<li><a href="">Jean</a></li>
-													<li><a href="">Khaki</a></li>
-												</ul>
-											</li>
+                                            <li>
+                                                <a href="">Jacket</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Jean</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Khaki</a>
+                                            </li>
+                                        </ul>
+                                        <div class="box-img">
+                                            <img src="/public/assets/img/men-logo.jpeg" alt="">
+                                        </div>
+                                    </div>
 
-											<li>
-												<div href="">Jacket</div>
-												<ul>
-													<li><a href="">Jean</a></li>
-													<li><a href="">Kaki</a></li>
-												</ul>
-											</li>
-										</ul>
-										<div class="box-img">
-											<img src="img/women-logo.jpg" alt="">
-										</div>
-									</div>
-								</li>
-								
-							</ul>
-						</div>
-					</div>
-					<div class="col-md-3 col-lg-3 ">
-						<div class="cover search-box">
-							<i class="fa fa-search" aria-hidden="true"></i>
-							<input class="search" type="search" placeholder="search">
-						</div>
-					</div>
-					<div class="col-md-2 col-lg-2 ">
-						<div class="cover account-box">
-							<a class="bag" href="cart.html">
-								<i class="fa fa-shopping-bag" aria-hidden="true"></i>
-								<span class="indicator">1</span>
-							</a>
-							
-							<button class="account" id="account"><i class="fa fa-sign-in" aria-hidden="true"></i> Log in</button>
-						</div>
-					</div>
-				</div>
-				<div class="hidden-desktop hidden-lage-screen row ">
-					<div class="col-xs-2 col-sm-2">
-						<div class="btn-bag">
-							<a href="cart.html">
-								<i class="fa fa-shopping-bag" aria-hidden="true"></i>
-							</a>
-						</div>
-					</div>
+                                </li>
+                                <li>
+                                    <a href="women.html">WOMEN</a>
+                                    <div class="menu-child">
+                                        <ul>
+                                            <li>
+                                                <a href="">T-Shirt</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Shirt</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Trousers</a>
+                                            </li>
 
-					<div class="col-xs-8 col-sm-8">
-						<div class="logo-shop">
-							<p>Fashion & <span>Style</span></p>
-						</div>
-					</div>
-					<div class="col-xs-2 col-sm-2">
-						<div class="btn-menu" id="btn-menu">
-							<i class="fa fa-bars" aria-hidden="true"></i>
-						</div>
-					</div>
-					<div class="mobile-menu" id="mobile-menu">
-						<ul>
-							<li><h3 class="title-menu">Categories</h3></li>
-							<li>
-								<div class="box-search">
-									<input type="text" name="" placeholder="search">
-									
-								</div>
-								
-							</li>
-							<li><h4>For Men</h4></li>
-							<li><a href="">T-Shirt</a></li>
-							<li><a href="">Shirt</a></li>
-							<li><a href="">Jacket</a></li>
-							<li><a href="">Trousers</a></li>
-							<li><a href="">Jean</a></li>
-							<li><a href="">Khaki</a></li>
-							<li><h4>For Women</h4></li>
-							<li><a href="">T-Shirt</a></li>
-							<li><a href="">Shirt</a></li>
-							<li><a href="">Jacket</a></li>
-							<li><a href="">Trousers</a></li>
-							<li><a href="">Jean</a></li>
-							<li><a href="">Khaki</a></li>
-							<li><a href="">Account</a></li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</header>
+                                            <li>
+                                                <a href="">Jacket</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Jean</a>
+                                            </li>
+                                            <li>
+                                                <a href="">Khaki</a>
+                                            </li>
+                                        </ul>
+                                        <div class="box-img">
+                                            <img src="/public/assets/img/women-logo.jpg" alt="">
+                                        </div>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-lg-3 ">
+                        <div class="cover search-box">
+                            <i class="fa fa-search" aria-hidden="true"></i>
+                            <input class="search" type="search" placeholder="search">
+                        </div>
+                    </div>
+                    <div class="col-md-2 col-lg-2 ">
+                        <div class="cover account-box">
+                            <a class="bag" href="cart.html">
+                                <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                                <span class="indicator">1</span>
+                            </a>
+
+                            <button class="account" id="account"><i class="fa fa-sign-in" aria-hidden="true"></i> Log in</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="hidden-desktop hidden-lage-screen row ">
+                    <div class="col-xs-2 col-sm-2">
+                        <div class="btn-bag">
+                            <a href="cart.html">
+                                <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                                <span class="indicator">1</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-8 col-sm-8">
+                        <div class="logo-shop">
+                            <a href="index.html"><p>Fashion & <span>Style</span></p></a>
+                        </div>
+                    </div>
+                    <div class="col-xs-2 col-sm-2">
+                        <div class="btn-menu" id="btn-menu">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+
+                        </div>
+                    </div>
+                    <div class="mobile-menu" id="mobile-menu">
+                        <ul>
+                            <li>
+                                <h3 class="title-menu">Categories</h3>
+                            </li>
+                            <li>
+                                <div class="box-search">
+                                    <input type="text" name="" placeholder="search">
+
+                                </div>
+
+                            </li>
+                            <li>
+                                <h4>For Men</h4>
+                            </li>
+                            <li><a href="product.html">T-Shirt</a></li>
+                            <li><a href="product.html">Shirt</a></li>
+                            <li><a href="product.html">Jacket</a></li>
+                            <li><a href="product.html">Trousers</a></li>
+                            <li><a href="product.html">Jean</a></li>
+                            <li><a href="product.html">Khaki</a></li>
+                            <li>
+                                <h4>For Women</h4>
+                            </li>
+                            <li><a href="product.html">T-Shirt</a></li>
+                            <li><a href="product.html">Shirt</a></li>
+                            <li><a href="product.html">Jacket</a></li>
+                            <li><a href="product.html">Trousers</a></li>
+                            <li><a href="product.html">Jean</a></li>
+                            <li><a href="product.html">Khaki</a></li>
+                            <li><a href="product.html">Account</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
---
+++ app/views/public/require/search.view.php
@@ -0,0 +1,29 @@
+<div class="col-lg-2 col-md-2 hidden-mobile hidden-tablet">
+<div class="menu-vertical">
+	<div class="box-title">Search</div>
+	<div class="content-box">
+		<label for="gender">Men or Women</label>
+		<select class="select" name="gender">
+		  <option value="men">Men</option>
+		  <option value="women">Women</option>
+	  </select>
+	  <label for="gender">Kinds</label>
+	  <select class="select" name="style">
+		  <option value="t-shirt">T-shirt</option>
+		  <option value="shirt">Shirt</option>
+		  <option value="pants">Pants</option>
+		  <option value="jacket">Jacket</option>
+	  </select>
+	  <label for="size">Size: </label>
+	  <select class="select" name="size">
+		  <option value="size-s">S</option>
+		  <option value="size-m">M</option>
+		  <option value="size-l">L</option>
+		  <option value="size-xl">XL</option>
+	  </select>
+	  <label>Price from: </label><input class="input-price price-from" type="text" name="">
+	  <label>Price to: </label><input class="input-price price-to" type="text" name="">
+	  <a href="product.html"><button class="btn-search">Search</button></a>
+  </div>
+</div>
+</div>
