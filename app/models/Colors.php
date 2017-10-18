e4126502fcb5c68dfc5686a9b6e97e74918d5a4b
Mai-Vo <mai.vv@neo-lab.vn>
2017-10-18 08:34:03 +0700

    - Color.php

--- app/models/Colors.php
+++ app/models/Colors.php
@@ -2,16 +2,11 @@
 namespace app\models;
 use core\App;
 use \PDO;
-<<<<<<< HEAD
-use core\Session;
-use core\database\QueryBuilder;
-use core\database\Connection;;
-=======
 use app\models\Model;
 use core\Session;
 use core\database\QueryBuilder;
 use core\database\Connection;
->>>>>>> f2f9b18accd9b089da57d2fd854fbc6d27b2134c
+
 
 class Colors extends Model
 {
