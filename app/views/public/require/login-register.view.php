65ac0eef809a3182f6c8034b0826218b64e6b90d
Mai-Vo <mai.vv@neo-lab.vn>
2017-10-20 09:42:51 +0700

    @ login-register.view.php

--- app/views/public/require/login-register.view.php
+++ app/views/public/require/login-register.view.php
@@ -13,25 +13,27 @@
 			<a class="forget-password" href="#">Forget password !</a>
 		</div>
 		<div class="register">
-			<div class="box-title">Register</div>
-			<input type="text" name="first-name" placeholder="First Name">
-			<input type="text" name="last-name" placeholder="Last Name">
-			<input type="text" name="user-name" placeholder="User Name">
-			<input type="email" name="email" placeholder="Email">
-			<input type="text" name="password" placeholder="Password">
-			<input type="text" name="re-password" placeholder="Re-Password">
-			<div class="gender">
-				<span>Genders</span>
-				<input id="male" type="radio" name="gender">
-				<label for="male">Male</label>
-				<input id="female" type="radio" name="gender">
-				<label for="female">Female</label>
-			</div>
-			<div class="birthday">
-				<span>Birthday</span>
-				<input type="date" name="birthday">
-			</div>
-			<button class="btn-register">Register</button>
+			<form action="" method="get" accept-charset="utf-8">
+				<div class="box-title">Register</div>
+				<input type="text" name="first-name" placeholder="First Name">
+				<input type="text" name="last-name" placeholder="Last Name">
+				<input type="text" name="user-name" placeholder="User Name">
+				<input type="email" name="email" placeholder="Email">
+				<input type="text" name="password" placeholder="Password">
+				<input type="text" name="re-password" placeholder="Re-Password">
+				<div class="gender">
+					<span>Genders</span>
+					<input id="male" type="radio" name="gender">
+					<label for="male">Male</label>
+					<input id="female" type="radio" name="gender">
+					<label for="female">Female</label>
+				</div>
+				<div class="birthday">
+					<span>Birthday</span>
+					<input type="date" name="birthday">
+				</div>
+				<button class="btn-register">Register</button>
+			</form>
 		</div>
 	</div>
 
